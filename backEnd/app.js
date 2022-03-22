5f3274905140ca7b01f7c8ffb1c076e5923dc53d
Nnaoyy <yoann.jamain@hotmail.fr>
2022-03-22 17:41:37 +0100

    backend

---
+++ backEnd/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const userRoutes = require ('./routes/user')
+const bodyParser = require('body-parser');
+const connection = require('./config/config.js');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(cors());
+
+
+
+connection.connect(function(err) {
+    if (err) throw err
+    console.log('You are now connected...');
+  });
+
+/*Permet l'accés à l'api depuis n'import quelle origine*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(express.json());
+
+app.use('/api/user', userRoutes);
+
+module.exports = app;
---
+++ backEnd/controllers/user.js
@@ -0,0 +1,55 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const connection = require('../config/config.js');
+
+
+exports.signup = (req, res) => {
+  let sql= 'select * from user where email=?';
+  connection.query(sql, [req.body.email], function (err, result) {
+    let user = result[0];
+    if (!user) {
+      bcrypt.hash(req.body.password, 8)
+      .then(hash => {
+          const user = ({
+              ...req.body,
+              password: hash
+          });
+          let sql = 'insert into user (nom, prenom, email,password) values (?,?,?,?)';
+          connection.query(sql, [user.lastName, user.firstName, user.email, user.password],
+           function(err,result){
+            if (err) throw err;
+            res.status(201).json({ message: `Utilisateur ${user.prenom} ajouté` });
+           })
+      })    
+      .catch(error => res.status(500).json(error));
+    } else {
+      res.status(401).json({ message: "Email déjà utilisée!" })
+    }
+  })
+  
+};
+
+exports.login = (req, res, next) => {
+  let sql= 'select * from user where email=?';
+  connection.query(sql, [req.body.email], function (err, result) {
+    let user = result[0];
+    if (!user) {
+      return res.status(401).json({ error: 'Utilisateur non trouvé !' });
+    }
+    bcrypt.compare(req.body.password, user.password)
+      .then(valid => {
+        if (!valid) {
+          return res.status(401).json({ error: 'Mot de passe incorrect !' });
+        }
+        res.status(200).json({
+          userId: user._id,
+          token: jwt.sign(
+            { userId: user._id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' }
+          )
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  })
+};
